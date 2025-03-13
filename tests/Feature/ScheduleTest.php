c9f163dc1bcc48386fb38ef63deae272b3259077
el <elopez@neusta.de>
2025-03-13 10:04:54 +0100

    readme, move report tests to the right location

--- tests/Feature/ScheduleTest.php
+++ tests/Feature/ScheduleTest.php
@@ -2,18 +2,22 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Console\Scheduling\Schedule;
 use Tests\TestCase;
 
 class ScheduleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_schedule_has_event(): void
     {
-        $response = $this->get('/');
+        $schedule = app(Schedule::class);
+        $events = $schedule->events();
+        $this->assertCount(1, $events);
+    }
 
-        $response->assertStatus(200);
+    public function test_schedule_has_event_again(): void
+    {
+        $schedule = app(Schedule::class);
+        $events = $schedule->events();
+        $this->assertCount(1, $events);
     }
 }
